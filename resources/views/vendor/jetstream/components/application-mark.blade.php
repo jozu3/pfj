8c4501eff324f58b7f45d9518ffc0f903f7fe135
Josué <josue.vitate@gmail.com>
2023-09-16 21:44:14 -0500

    logo

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,4 +1,4 @@
-<img src="{{ config('app.url', 'http://localhost/pfj/public').'/img/pfj-lima-norte.png' }}" style="
+<img src="{{ config('app.url', 'http://localhost/pfj/public').'/img/logo_pfj2024.png' }}" style="
 							   	width: 75px
 							    " 
 		alt="">
