7efa56768cf67eac5935aa0f5d03aefe52166c80
Josué <josue.vitate@gmail.com>
2023-09-16 21:46:12 -0500

    logo

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,4 +1,4 @@
-<img src="{{ config('app.url', 'http://localhost/pfj/public').'/img/logo_pfj2024.png' }}" style="
+<img src="{{ config('app.url', 'http://localhost/pfj/public').'/img/logo-circle-2024.png' }}" style="
 							   	width: 75px
 							    " 
 		alt="">
